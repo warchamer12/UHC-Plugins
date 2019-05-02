7071d138b01854e0ce5eb28d14467a86ef541820
Cyramek <xcyramek@gmail.com>
2019-05-02 15:32:47 +0200

    Poprawiono ArenaTimerTask

--- UHC-Bukkit/src/main/kotlin/net/warchamer12/uhc/tasks/ArenaTimerTask.kt
+++ UHC-Bukkit/src/main/kotlin/net/warchamer12/uhc/tasks/ArenaTimerTask.kt
@@ -1,14 +1,23 @@
 package net.warchamer12.uhc.tasks
 
+import net.warchamer12.uhc.border.UHCApiBorder
+import org.bukkit.Bukkit
+
 class ArenaTimerTask : Runnable {
 
-    private var minutes = 40
+    private val border = UHCApiBorder.getAPI().createBorder(Bukkit.getWorlds()[0], Bukkit.getWorlds()[0].worldBorder)
+    private var minutes = 50
 
     override fun run() {
-        if (minutes == 30) {
-            //Wlaczenie pvp
-        } else if (minutes == 0) {
-            //teleportowanie do deathmetchu
+        when (minutes) {
+            50 -> border.moveWorldBorder(0.0, 0.0, 1000.0, 0, 0.0)
+            40 -> border.moveWorldBorder(0.0, 0.0, 800.0, 120, 1.0)
+            30 -> border.moveWorldBorder(0.0, 0.0, 600.0, 120, 1.0)
+            20 -> border.moveWorldBorder(0.0, 0.0, 400.0, 120, 1.0)
+            10 -> border.moveWorldBorder(0.0, 0.0, 200.0, 120, 1.0)
+            0 -> {
+                //deathmatch
+            }
         }
 
         minutes--
