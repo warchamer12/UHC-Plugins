17a1ef9a5f9e9ad755b6e2aab9a98d6d3da7377a
Cyramek <xcyramek@gmail.com>
2019-05-02 15:12:48 +0200

    Poprawiono ArenaTimerTask

--- UHC-Bukkit/src/main/kotlin/net/warchamer12/uhc/tasks/ArenaTimerTask.kt
+++ UHC-Bukkit/src/main/kotlin/net/warchamer12/uhc/tasks/ArenaTimerTask.kt
@@ -2,16 +2,16 @@ package net.warchamer12.uhc.tasks
 
 class ArenaTimerTask : Runnable {
 
-    private var minutes = 0
+    private var minutes = 40
 
     override fun run() {
-        if (minutes == 10) {
+        if (minutes == 30) {
             //Wlaczenie pvp
-        } else if (minutes == 40) {
+        } else if (minutes == 0) {
             //teleportowanie do deathmetchu
         }
 
-        minutes++
+        minutes--
     }
 
 }
