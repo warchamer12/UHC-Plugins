e6bf4669ddd76918944a84ba4cc6990c29a970de
BuildTools <unconfigured@null.spigotmc.org>
2019-05-02 19:55:21 +0200

    UHC

--- UHC-Bukkit/src/main/java/net/warchamer12/uhc/listeners/JoinListener.java
+++ UHC-Bukkit/src/main/java/net/warchamer12/uhc/listeners/JoinListener.java
@@ -2,21 +2,43 @@ package net.warchamer12.uhc.listeners;
 
 import net.warchamer12.uhc.UHCPlugin;
 
+import net.warchamer12.uhc.utils.ScoreboardBuilder;
+import net.warchamer12.uhc.utils.Util;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class JoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        ScoreboardBuilder sidebar = new ScoreboardBuilder(Util.fixColor("&e&lUHC CHAMPIONS"));
 
+        Date teraz = new Date();
+        SimpleDateFormat data = new SimpleDateFormat("dd/MM/yyyy");
+        sidebar.add(Util.fixColor("&7" + data.format(teraz)), -1);
+        sidebar.add(" ", -2);
+        for(Player online : Bukkit.getServer().getOnlinePlayers()) {
+            sidebar.add(Util.fixColor("&fGracze: &a" + online + "/70"), -3);
+        }
+        sidebar.add(" ", -4);
+        sidebar.add(Util.fixColor("&fRozgrywka wystartuje za &a" + startArea, -5));
+        sidebar.add(Util.fixColor("&fjesli dolaczy jeszcze &a" + more + " &fgraczy!", -6));
+        sidebar.add(" ", -7);
+        sidebar.add(Util.fixColor("&fTryb: &6&l" + modeGame), -8);
+        sidebar.add("&eTutaj bedzie domena!", -9);
         new BukkitRunnable() {
             @Override
             public void run() {
+                sidebar.build();
+                sidebar.send(player);
             }
         }.runTaskLater(UHCPlugin.Companion.getInstance(), 3L);
     }
---
+++ UHC-Bukkit/src/main/java/net/warchamer12/uhc/utils/ScoreboardBuilder.java
@@ -0,0 +1,89 @@
+package net.warchamer12.uhc.utils;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import com.google.common.collect.MapConstraint;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+import java.util.AbstractMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class ScoreboardBuilder {
+
+    private Scoreboard scoreboard;
+    private String title;
+    private List<Object> teams;
+
+    public ScoreboardBuilder(String title) {
+        this.scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        this.title = title;
+        this.teams = (List<Object>) Lists.newArrayList();
+    }
+
+    public void blankLine() {
+        this.add(" ");
+    }
+
+    public void add(String text) {
+        this.add(text);
+    }
+
+    @SuppressWarnings("unused")
+    private Map.Entry<Team, String> createTeam(final String text) {
+        String result = "";
+        if (text.length() <= 16) {
+            return new AbstractMap.SimpleEntry<Team, String>(null, text);
+        }
+        final Team team = this.scoreboard.registerNewTeam("text-" + this.scoreboard.getTeams().size());
+        final Iterator<String> iterator = Splitter.fixedLength(16).split((CharSequence) text).iterator();
+        team.setPrefix((String) iterator.next());
+        result = iterator.next();
+        if (text.length() > 32) {
+            team.setSuffix((String) iterator.next());
+        }
+        this.teams.add(team);
+        return new AbstractMap.SimpleEntry<Team, String>(team, result);
+    }
+
+    public void add(String text, final Integer score) {
+        Preconditions.checkArgument(text.length() < 48, (Object) "text cannot be over 48 characters in length");
+    }
+
+    public void build() {
+        Objective obj = this.scoreboard.registerNewObjective(this.title, "dummy");
+        obj.setDisplayName(this.title);
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+    }
+
+    public void reset() {
+        this.title = null;
+        for (final Object t : this.teams) {
+            ((Team) t).unregister();
+        }
+        this.teams.clear();
+    }
+
+    public Scoreboard getScoreboard() {
+        return this.scoreboard;
+    }
+
+    public static void destroy(Player player) {
+        ScoreboardBuilder sidebar = new ScoreboardBuilder("a");
+        sidebar.build();
+        sidebar.send(player);
+    }
+
+    public void send(Player player) {
+        player.setScoreboard(this.scoreboard);
+    }
+
+
+}
