6e7f40425995684d83f9f0b166461dd2db09b365
BuildTools <unconfigured@null.spigotmc.org>
2019-05-07 16:24:12 +0200

    UHC

--- UHC-Bukkit/src/main/java/net/warchamer12/uhc/commands/WaitingRoomCommand.java
+++ UHC-Bukkit/src/main/java/net/warchamer12/uhc/commands/WaitingRoomCommand.java
@@ -2,6 +2,7 @@ package net.warchamer12.uhc.commands;
 
 import net.warchamer12.uhc.UHCPlugin;
 import net.warchamer12.uhc.utils.Util;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,7 +14,8 @@ public class WaitingRoomCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
             sender.sendMessage(Util.fixColor("&6Utworzono waiting room!"));
-            UHCPlugin.Companion.getInstance().getConfig().set("WaitingRoom", 1);
+            Player player = (Player) ((Player) sender).getLocation();
+            UHCPlugin.Companion.getInstance().getConfig().set("WaitingRoom", player);
             UHCPlugin.Companion.getInstance().saveConfig();
         } else {
             sender.sendMessage("Uzycie tej komendy nie jest możliwe z poziomu konsoli!");
