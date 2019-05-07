5a7ed4eeeb08c5966c43fa6a933c33930f5f2eff
BuildTools <unconfigured@null.spigotmc.org>
2019-05-07 12:29:57 +0200

    UHC

--- UHC-Bukkit/src/main/java/net/warchamer12/uhc/DeathListener.java
+++ UHC-Bukkit/src/main/java/net/warchamer12/uhc/DeathListener.java
@@ -14,7 +14,7 @@ public class DeathListener implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         if (event.getEntity() instanceof Player && event.getEntity().getKiller() instanceof Player) {
-            Player zabity = (Player) event.getEntity();
+            Player zabity =  event.getEntity();
             JoinListener.UHCPlayers.remove(zabity);
             JoinListener.UHCDeathPlayers.add(zabity);
             Player zabojca = zabity.getKiller();
@@ -22,8 +22,29 @@ public class DeathListener implements Listener {
             zabity.setGameMode(GameMode.ADVENTURE);
             zabity.setFlying(true);
             zabity.setFlySpeed(4);
+            new Title(zabity).title(Util.fixColor("&cUmarles!")).times(1, 3, 1).send();
             for (Player UHCPlayers : JoinListener.UHCPlayers) {
-                new Title(zabity).title(Util.fixColor("&cUmarles!")).times(1, 3, 1).send();
+                zabity.hidePlayer(UHCPlayers);
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        if (JoinListener.Game == false) {
+                            zabity.showPlayer((Player) JoinListener.UHCDeathPlayers);
+                        } else {
+                            cancel();
+                        }
+                    }
+                }.runTaskLater(UHCPlugin.Companion.getInstance(), 30L);
+            }
+        } else {
+            Player zabity = event.getEntity();
+            JoinListener.UHCPlayers.remove(zabity);
+            JoinListener.UHCDeathPlayers.add(zabity);
+            zabity.setGameMode(GameMode.ADVENTURE);
+            zabity.setFlying(true);
+            zabity.setFlySpeed(4);
+            new Title(zabity).title(Util.fixColor("&cUmarles!")).times(1, 3, 1).send();
+            for (Player UHCPlayers : JoinListener.UHCPlayers) {
                 zabity.hidePlayer(UHCPlayers);
                 new BukkitRunnable() {
                     @Override
